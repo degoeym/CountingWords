b1fb6025bb1860415dbcbc2fbda0968c799299cb
degoeym <degoeym@gmail.com>
2014-10-11 02:38:12 -0400

    Final solution

--- code.rb
+++ code.rb
@@ -1,42 +1,14 @@
-=begin
-File.open('speech.txt') do |f|
-    f.each_line do |line|
-        words = line.split(/\s+/)
-        count = Hash.new(0)
-        words.each { |word| count[word] += 1 }
-        count = count.sort_by { |x,y| y }
-        count.reverse!
-        count.each { |word, count| puts count.to_s + "-" + word }
-    end
-end
-=begin
-File.open('speech.txt') do |f|
-	f.each_line do |line|
-		words = line.split(/\s+/).length
-		count = Hash.new(0)
-		words.each { |word| count[word] += 1 }
-		count = count.sort_by { |x,y| y }
-		count.reverse!
-		count.each { |word, count| puts count.to_s + "-" + word }
-	end
-end
-=end
-
-File.readlines('speech.txt').each do |line|
-	words = line.split(/\W+/)
+def read_file(file_name)
 	count = Hash.new(0)
-	words.each { |w| count[word] += 1 }
+
+	File.readlines(file_name).each do |line|
+		words = line.split(/\W+/)
+		words.each { |w| count[w] += 1 }
+	end
+
 	count = count.sort_by { |x, y| y }
-	count.reverse
+	count.reverse!
 	count.each { |word, count| puts count.to_s + "-" + word }
 end
 
-=begin
-words.each do |word|
-	count = Hash.new(0)
-	count[word] += 1
-	count = count.sort_by { |x,y| y }
-	count.reverse
-	count.each { |word, count| puts count.to_s + "-" + word }[0..20]
-end
-=end
+read_file('speech.txt')
